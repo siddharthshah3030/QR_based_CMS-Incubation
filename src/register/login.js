b78a8f0d3db6432042e8ab528796d01bbea740ad
siddharth shah <siddharthshah3030@gmail.com>
2019-02-14 17:35:36 +0530

    WIP post request

--- src/register/login.js
+++ src/register/login.js
@@ -9,10 +9,41 @@ class Login extends Component {
 constructor(props){
   super(props);
   this.state={
-  username:'user',
+  username:'',
   password:''
   }
- }
+
+}
+ handleClick(event){
+        let url = "/login"
+       let data = {
+        "email":this.state.username,
+        "password":this.state.password
+        }
+        console.log("login button pressed")
+        fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
+        .then(
+                response => response.json(),
+            console.log(JSON.stringify())
+                
+            
+            
+        ) // parses response to JSON
+       .catch(error => console.error(error));
+}
+
 render() {
     return (
       <div>
@@ -54,32 +85,3 @@ export default Login;
 
 
 
-// handleClick(event){
-//     var apiBaseUrl = "http://localhost:4000/api/";
-//     var self = this;
-//     var payload={
-//     "email":this.state.username,
-//     "password":this.state.password
-//     }
-//     axios.post(apiBaseUrl+'login', payload)
-//     .then(function (response) {
-//     console.log(response);
-//     if(response.data.code == 200){
-//     console.log("Login successfull");
-//     var uploadScreen=[];
-//     uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-//     self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-//     }
-//     else if(response.data.code == 204){
-//     console.log("Username password do not match");
-//     alert("username password do not match")
-//     }
-//     else{
-//     console.log("Username does not exists");
-//     alert("Username does not exist");
-//     }
-//     })
-//     .catch(function (error) {
-//     console.log(error);
-//     });
-//     }
