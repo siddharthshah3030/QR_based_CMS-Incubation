ced3ccfd58645e0632a559e3d8147ef21b4773ee
siddharth shah <siddharthshah3030@gmail.com>
2019-02-17 05:02:28 +0530

    git merge from navin branch

--- src/components/rental/rentallist.js
+++ src/components/rental/rentallist.js
@@ -1,108 +1,141 @@
-import React from 'react';
-import burl from '../../url';
-import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
-    CardSubtitle, CardBody } from 'reactstrap';
+import React from "react";
+import burl from "../../url";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
 
 class CategoryList extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-        data: ""
-    }
+    this.state = {
+      data: []
+    };
     // this.handlePassword = this.handlePassword.bind(this);
     // this.handleUsername= this.handleUsername.bind(this);
-  
   }
 
-componentWillMount() {
-    console.log("making fetch request")
-  fetch(burl +"/api/resources/list/", {
-    method: 'get',
-    headers: {
-      // 'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-      'Authorization': 'Token ad309027364a277e66eb3bf325e826dadf477704'
-
-    },
-  }).then(res=>res.json())
-    .then(res =>{
-      console.log("fetching data")
-    //   console.log(res);
-      this.setState({
-
-          data : res
+  componentWillMount() {
+    console.log("making fetch request");
+    fetch(burl + "/api/resources/list/", {
+      method: "get",
+      headers: {
+        // 'Accept': 'application/json, text/plain, */*',
+        "Content-Type": "application/json",
+        Authorization: "Token ad309027364a277e66eb3bf325e826dadf477704"
       }
-      )
     })
-    .catch(function(e) {
+      .then(res => res.json())
+      .then(res => {
+        console.log("fetching data");
+        //   console.log(res);
+        this.setState({
+          data: res
+        });
+      })
+      .catch(function(e) {
         // console.log(burl); // "oh, no!"
 
-      console.log(e); // "oh, no!"
-    })
-}
+        console.log(e); // "oh, no!"
+      });
+  }
 
-render() {
-    console.log(this.state.data)
-  var style = {
-    // color: "white",
-    // fontSize: 200,
-    
-    // marginTop: 100,
-    // paddingTop: 100
-  };
-  var str = JSON.stringify(this.state.dat, null, 4); // (Optional) beautiful indented output.
-console.log(this.state.dat); // Logs output to dev tools console.
-  // console.log(typeof(this.state.data[0]))
-
-
-  return (
-    <div style={style}>
- <CardGroup>
-{/* 
-   { this.state.data.forEach(function(element) {
-  console.log(element);
-})} */}
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+  render() {
+    console.log(this.state.data);
+    var style = {
+      // color: "white",
+      // fontSize: 200,
+      // marginTop: 100,
+      // paddingTop: 100
+    };
+    var str = JSON.stringify(this.state.dat, null, 4); // (Optional) beautiful indented output.
+    console.log(this.state.dat); // Logs output to dev tools console.
+    // console.log(typeof(this.state.data[0]))
+    var categories = this.state.data.map((ele,i) => (
+      <Card key={i}>
+        <CardImg
+          top
+          width="100%"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+          alt="Card image cap"
+        />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
+          <CardTitle>{ele.name}</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardText>{ele.description}
+          </CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+    ));
 
-    </CardGroup>
-  
-</div>
-  )
-}
+    return (
+      <div style={style}>
+        {categories}
+        {/* <CardGroup>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </CardGroup> */}
+      </div>
+    );
+  }
 }
 export default CategoryList;
