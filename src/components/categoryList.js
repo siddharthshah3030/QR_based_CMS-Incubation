d669aeb8c74ed63039417d4ab4c8597fd7a3f530
siddharth shah <siddharthshah3030@gmail.com>
2019-02-16 23:18:31 +0530

    adding one checkbos

--- src/components/categoryList.js
+++ src/components/categoryList.js
@@ -3,6 +3,8 @@ import burl from '../url';
 import React from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
+import { Table } from 'reactstrap';
+import { CustomInput, Form, FormGroup, Label } from 'reactstrap';
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -44,11 +46,47 @@ export default class Example extends React.Component {
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
           <TabPane tabId="1">
+
+
             <Row>
               <Col sm="12">
                 <h4>Tab 1 Contents</h4>
               </Col>
             </Row>
+
+            <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td><CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
           </TabPane>
           <TabPane tabId="2">
             <Row>
