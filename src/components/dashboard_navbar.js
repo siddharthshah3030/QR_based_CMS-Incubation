07c3084172b008259db770dce18c119ee425068e
naveennvrgup <naveennvrgup@gmail.com>
2019-02-17 02:08:21 +0530

    merged with master

---
+++ src/components/dashboard_navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+
+class menubar extends Component {
+    sign_out_handler = (e) => {
+        e.preventDefault()
+
+        localStorage.removeItem('token')
+        localStorage.removeItem('uid')
+        this.props.history.push('/')
+    }
+    
+    render() {
+        return (
+            <div>
+                <nav class="nav-wrapper">
+                    <div className="container">
+                        <div className="cnav">
+                            <NavLink className="" to="/dashboard/visitor/">Visitor</NavLink>
+                            <NavLink className="" to="/dashboard/library/">Librarian</NavLink>
+                            <NavLink className="" to="/dashboard/caffeteria/">Caffeteria</NavLink>
+                            <NavLink className="" onClick={this.sign_out_handler} to="/staff/price/">Signout</NavLink>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default withRouter(menubar)
