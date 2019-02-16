6dae7ce118c96948a6e692c8eb5b9b1d479ea0fc
siddharth shah <siddharthshah3030@gmail.com>
2019-02-16 22:24:05 +0530

    WIP changes

--- src/components/rentList.js
+++ src/components/rentList.js
@@ -8,6 +8,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Checkbox from '@material-ui/core/Checkbox';
 import Avatar from '@material-ui/core/Avatar';
+import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
   root: {
@@ -42,25 +43,57 @@ class CheckboxListSecondary extends React.Component {
     const { classes } = this.props;
 
     return (
+      <div>
+
       <List dense className={classes.root}>
-        {[0, 1, 2, 3].map(value => (
+                <ListItem key={0} button>
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar n°${0 + 1}`}
+                src={`/static/images/avatar/${0 + 1}.jpg`}
+                />
+            </ListItemAvatar>
+            <ListItemText primary={`Line item ${0 + 1}`} />
+            <ListItemText primary={`description ${0 + 1}`} />
+            <ListItemText primary={`description ${0 + 1}`} />
+            <ListItemSecondaryAction>
+              <Checkbox
+                onChange={this.handleToggle(0)}
+                checked={this.state.checked.indexOf(0) !== -1}
+                />
+            </ListItemSecondaryAction>
+          </ListItem>
+        {[1, 2, 3, 4].map(value => (
           <ListItem key={value} button>
             <ListItemAvatar>
               <Avatar
                 alt={`Avatar n°${value + 1}`}
                 src={`/static/images/avatar/${value + 1}.jpg`}
-              />
+                />
             </ListItemAvatar>
+            <ListItemText primary={`Line item ${value + 1}`} />
+            <ListItemText primary={`Line item ${value + 1}`} />
             <ListItemText primary={`Line item ${value + 1}`} />
             <ListItemSecondaryAction>
               <Checkbox
                 onChange={this.handleToggle(value)}
                 checked={this.state.checked.indexOf(value) !== -1}
-              />
+                />
             </ListItemSecondaryAction>
           </ListItem>
         ))}
       </List>
+
+
+<Button
+variant="contained"
+color="primary"
+onClick={() => this.props.history.push('/dashboard/orderconfirm')}
+>
+Book Them
+</Button>
+      
+</div>
     );
   }
 }
